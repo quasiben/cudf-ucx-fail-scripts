e464cde12c640637bf183391c041ad9d90297b18
Ashwin Srinath <shwina@users.noreply.github.com>
2019-08-29 15:45:29 -0700

    Update dummy-column.ipynb and dummy.py

--- dummy.py
+++ dummy.py
@@ -3,38 +3,35 @@ from distributed.protocol.cuda import cuda_serialize, cuda_deserialize
 import numpy as np
 import cupy
 
+from librmm_cffi import librmm as rmm
+
 class DummyColumn(object):
 
-    def __init__(self):
-        import threading
-        self.lock = threading.Lock()
-        pass
+    def __init__(self, size, kind):
+        self.size = size
+        self.kind = kind
 
     def serialize(self):
         header = {}
-        frames = [cuda.to_device(np.arange(100000))]
-        # frames = [cupy.asarray(np.arange(100000))] # <- failing at size 100000
+        if self.kind == "numba":
+            frames = [cuda.to_device(np.arange(self.size))]
+        elif self.kind == "rmm":
+            frames = [rmm.to_device(np.arange(self.size))]
+        elif self.kind == "cupy":
+            frames = [cupy.asarray(np.arange(self.size))]
+        else:
+            frames = [np.arange(self.size)]
         return header, frames
 
     @classmethod
     def deserialize(cls, header, frames):
-        return np.array([1, 2, 3])
+        return None
 
 @cuda_serialize.register(DummyColumn)
 def serialize(col):
-    print("I AM A DUMMY")
     return col.serialize()
 
 @cuda_deserialize.register(DummyColumn)
 def deserialize(header, frames):
     (frame,) = frames
-    # TODO: put this in ucx... as a kind of "fixup"
-#     breakpoint()
-#     try:
-#         frame.typestr = '<i8'
-#         frame.shape = (3,)
-#     except AttributeError:
-#         pass
-#     arr = cupy.asarray(frame)
-#     print(arr, type(arr))
     return DummyColumn.deserialize(header, frames)
